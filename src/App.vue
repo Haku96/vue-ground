bc6216babcb3cdbf93d3d4b86d437d22f02b7525
Haku96 <1424478294@qq.com>
2025-09-17 23:47:55 +0800

    Add App.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-config-provider :locale="zhCn"> Hello Vue.js! </el-config-provider>
+</template>
+
+<script lang="ts" setup>
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+import axios from 'axios'
+
+const login = async () => {
+  const { data } = await axios.post('/api/user/login', {
+    username: 'admin',
+    password: '123456',
+  })
+  console.log(data)
+}
+
+login()
+</script>
