e36b7ef4a4bc9cfc3b182773ee7e989b7a3fb81f
Haku96 <1424478294@qq.com>
2025-09-17 23:49:17 +0800

    Add counter store

---
+++ src/stores/counter.ts
@@ -0,0 +1,18 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+interface CountStore {
+  count: number
+  doubleCount: number
+  increment: () => void
+}
+
+export const useCounterStore = defineStore<'counter', CountStore>('counter', () => {
+  const count = ref(0)
+  const doubleCount = computed(() => count.value * 2)
+  function increment() {
+    count.value++
+  }
+
+  return { count, doubleCount, increment }
+})
